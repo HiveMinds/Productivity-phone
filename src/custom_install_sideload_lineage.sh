de05049d9aa9391f8f148717803f9e67e3abcb03
a-t-0 <eai@protonmail.com>
2021-05-05 00:48:51 +0200

    Included link to zip of Fairphone Open OS.

--- src/custom_install_sideload_lineage.sh
+++ src/custom_install_sideload_lineage.sh
@@ -8,11 +8,15 @@ apt_update() {
 	device_id="${adb_devices:25:8}"
 	
 	# Determine which os is selected.
-	if [ "$OS_CHOICE" == "fairphone" ]; then
-		os_filepath="$FAIRPHONE_FILEPATH"
+	if [ "$OS_CHOICE" == "fairphone_os" ]; then
+		os_filepath="$FAIRPHONE_OS_FILEPATH"
 		os_url="$FAIRPHONE_OS_URL"
-		expected_md5_of_os_zip="$FAIRPHONE_MD5"
-	elif [ "$OS_CHOICE" == "fairphone" ]; then
+		expected_md5_of_os_zip="$FAIRPHONE_OS_MD5"
+	elif [ "$OS_CHOICE" == "fairphone_open" ]; then
+		os_filepath="$FAIRPHONE_OPEN_FILEPATH"
+		os_url="$FAIRPHONE_OPEN_URL"
+		expected_md5_of_os_zip="$FAIRPHONE_OPEN_MD5"
+	elif [ "$OS_CHOICE" == "lineage" ]; then
 		os_filepath="$LINEAGEOS_FILEPATH"
 		os_url="$LINEAGEOS_URL"
 		expected_md5_of_os_zip="$LINEAGEOS_MD5"
